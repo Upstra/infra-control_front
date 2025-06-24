11ae038c969b14797ea5c43957b332f2da581449
James ABIB <61329786+jabibamman@users.noreply.github.com>
2025-06-24 09:44:23 +0200

    feat(ci): add sonarcloud and coverage (#17)

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,9 @@ import { fileURLToPath, URL } from 'node:url';
 export default defineConfig({
   test: {
     environment: 'jsdom',
+    coverage: {
+      reporter: ['text', 'lcov'],
+    },
   },
   resolve: {
     alias: {
