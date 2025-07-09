9a854a6fa8d217fd7889c71a4c6ffb2116462024
James ABIB <james.bidule@gmail.com>
2025-07-09 23:28:21 +0200

    feat: integrate user preferences in UPS and Room list views

--- src/features/settings/api.ts
+++ src/features/settings/api.ts
@@ -3,18 +3,18 @@ import type { UserPreferences, UserPreferencesUpdateDto } from './types';
 
 export const preferencesApi = {
   getPreferences(): Promise<UserPreferences> {
-    return axios.get('/user/me/preferences').then((response) => response.data);
+    return axios.get('/users/me/preferences').then((response) => response.data);
   },
 
   updatePreferences(data: UserPreferencesUpdateDto): Promise<UserPreferences> {
     return axios
-      .patch('/user/me/preferences', data)
+      .patch('/users/me/preferences', data)
       .then((response) => response.data);
   },
 
   resetPreferences(): Promise<UserPreferences> {
     return axios
-      .post('/user/me/preferences/reset')
+      .post('/users/me/preferences/reset')
       .then((response) => response.data);
   },
 };
