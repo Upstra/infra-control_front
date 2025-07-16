90fe6c938b0a27d66ef8c3ba5a758ef6de4f576b
James ABIB <james.bidule@gmail.com>
2025-07-16 20:42:49 +0200

    fix: add ip

--- start-pm2.sh
+++ start-pm2.sh
@@ -9,7 +9,7 @@ echo -e "${green}Deleting old PM2 processes...${reset}"
 pnpm pm2:stop
 
 echo -e "${green}Building application...${reset}"
-VITE_API_URL=http://localhost:8080 pnpm build
+VITE_API_URL=http://172.23.50.2:8080 pnpm build
 
 echo -e "${green}Starting with PM2 on port 8080...${reset}"
 pm2 start ecosystem.config.cjs
